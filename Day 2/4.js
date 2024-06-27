ceaab2895b9dcfae79524ae0cbdff790b4fb83bc
Jotaniya-Darshit <jotaniyadarshit1235@gmail.com>
2024-06-27 17:17:09 +0530

    Data Types & Variables

---
+++ Day 2/4.js
@@ -0,0 +1,6 @@
+var a = 3
+console.log("A = ",a)
+var b = 5
+console.log("B = ",b)
+var c = a + b
+console.log("3 + 5 = ",c)
---
+++ Day 2/5.js
@@ -0,0 +1,6 @@
+var l = 13
+console.log("Length = ", l)
+var w = 6
+console.log("Width = ",w)
+var area = l * w
+console.log('Area of Rectangle = ',area)
