ec6ac94abac65ee509e6dad49deed1f25699b8a8
Jotaniya-Darshit <jotaniyadarshit1235@gmail.com>
2024-07-31 16:32:14 +0530

    Conditional Statement Project

---
+++ Day 13/3.js
@@ -0,0 +1,15 @@
+// 3. Write a simple number guessing game. Provide a secret number and a guess. Based on those numbers give players clues 
+// if their guess is higher, lower or correct.
+let val = 25;
+if(val> 20)
+    {
+    console.log("Your guess is too high");
+    }   
+    else if(val <=10)
+    {
+    console.log("Your guess is too low");
+    }
+    else
+    {
+    console.log("Congratulations! Your guess is correct");
+    }
